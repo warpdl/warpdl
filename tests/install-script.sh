ddad63e4951323d0071a261385cd75c4d37830ff
Divanshu Chauhan <divkix@divkix.me>
2023-05-31 20:25:36 +0530

    test: remove '-it'

--- tests/install-script.sh
+++ tests/install-script.sh
@@ -4,16 +4,16 @@ set -e
 
 # deb
 echo "Testing ubuntu"
-time docker run --rm -it -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro ubuntu /usr/warpdl/cli/ubuntu-debian.sh
+time docker run --rm -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro ubuntu /usr/warpdl/cli/ubuntu-debian.sh
 echo "Testing debian"
-time docker run --rm -it -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro debian /usr/warpdl/cli/ubuntu-debian.sh
+time docker run --rm -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro debian /usr/warpdl/cli/ubuntu-debian.sh
 
 # rpm
 echo "Testing centos"
-time docker run --rm -it -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro centos /usr/warpdl/cli/fedora-centos.sh
+time docker run --rm -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro centos /usr/warpdl/cli/fedora-centos.sh
 echo "Testing fedora"
-time docker run --rm -it -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro fedora /usr/warpdl/cli/fedora-centos.sh
+time docker run --rm -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro fedora /usr/warpdl/cli/fedora-centos.sh
 
 # apk
 echo "Testing alpine"
-time docker run --rm -it -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro alpine /usr/warpdl/cli/alpine.sh
+time docker run --rm -v "$(pwd)/tests/distros":/usr/warpdl/cli:ro alpine /usr/warpdl/cli/alpine.sh
