5d7cb6daf965bd78c74526cbe188a559ca7078e0
Divanshu Chauhan <divkix@divkix.me>
2023-06-01 18:19:32 +0530

    update to 0.0.26

--- warp.go
+++ warp.go
@@ -285,7 +285,7 @@ func main() {
 		Name:                  "Warp",
 		HelpName:              "warp",
 		Usage:                 "An ultra fast download manager.",
-		Version:               "v0.0.24", // NOTE: change version from here
+		Version:               "v0.0.26", // NOTE: change version from here
 		UsageText:             "warp <command> [arguments...]",
 		Description:           Description,
 		CustomAppHelpTemplate: HELP_TEMPL,
