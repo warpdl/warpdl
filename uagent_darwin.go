06e32b428dc3d219b4f500ef86e24d5b78f7e871
anonyindian <anonyindian1@gmail.com>
2023-06-30 18:18:21 +0530

    make windows user agents as default

--- uagent_darwin.go
+++ uagent_darwin.go
@@ -2,8 +2,7 @@ package main
 
 import "github.com/warpdl/warplib"
 
-var UserAgents = map[string]string{
-	"warp":    warplib.DEF_USER_AGENT,
-	"firefox": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13.4; rv:109.0) Gecko/20100101 Firefox/114.0",
-	"chrome":  "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+func init() {
+	UserAgents["firefox"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 13.4; rv:109.0) Gecko/20100101 Firefox/114.0"
+	UserAgents["chrome"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
--- uagent_linux.go
+++ uagent_linux.go
@@ -1,9 +1,6 @@
 package main
 
-import "github.com/warpdl/warplib"
-
-var UserAgents = map[string]string{
-	"warp":    warplib.DEF_USER_AGENT,
-	"firefox": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
-	"chrome":  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+func init() {
+	UserAgents["firefox"] = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0"
+	UserAgents["chrome"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
