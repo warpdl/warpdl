16831370f6f17865305545551444f62e86a2f2e9
celestix <i@anodite.me>
2024-06-21 17:59:56 +0000

    daemon: temp: remove credman

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -2,52 +2,35 @@ package cmd
 
 import (
 	"log"
-	"path/filepath"
 
 	"github.com/urfave/cli"
 	"github.com/warpdl/warpdl/cmd/common"
 	"github.com/warpdl/warpdl/internal/api"
 	"github.com/warpdl/warpdl/internal/extl"
 	"github.com/warpdl/warpdl/internal/server"
-	"github.com/warpdl/warpdl/pkg/credman"
-	"github.com/warpdl/warpdl/pkg/credman/keyring"
-	"github.com/warpdl/warpdl/pkg/warplib"
 )
-
-// s, err := api.NewApi(l)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &Daemon{
-// 		l:   l,
-// 		api: s,
-// 	}, nil
-// 	serv := server.NewServer(d.l)
-// 	d.api.RegisterHandlers(serv)
-// 	return serv.Start()
 
 func daemon(ctx *cli.Context) error {
 	l := log.Default()
-	
-	kr := keyring.NewKeyring()
-	key, err := kr.GetKey()
-	if err != nil {
-		key, err = kr.SetKey()
-		if err != nil {
-			common.PrintRuntimeErr(ctx, "daemon", "keyring", err)
-			return nil
-		}
-	}
-	
-	cookieFile := filepath.Join(warplib.ConfigDir, "cookies.warp")
-	cm, err := credman.NewCookieManager(cookieFile, key)
-	if err != nil {
-		common.PrintRuntimeErr(ctx, "daemon", "credman", err)
-		return nil
-	}
-	defer cm.Close()
 
-	
+	// kr := keyring.NewKeyring()
+	// key, err := kr.GetKey()
+	// if err != nil {
+	// 	key, err = kr.SetKey()
+	// 	if err != nil {
+	// 		common.PrintRuntimeErr(ctx, "daemon", "keyring", err)
+	// 		return nil
+	// 	}
+	// }
+
+	// cookieFile := filepath.Join(warplib.ConfigDir, "cookies.warp")
+	// cm, err := credman.NewCookieManager(cookieFile, key)
+	// if err != nil {
+	// 	common.PrintRuntimeErr(ctx, "daemon", "credman", err)
+	// 	return nil
+	// }
+	// defer cm.Close()
+
 	elEng, err := extl.NewEngine(l, false)
 	if err != nil {
 		common.PrintRuntimeErr(ctx, "daemon", "extloader_engine", err)
